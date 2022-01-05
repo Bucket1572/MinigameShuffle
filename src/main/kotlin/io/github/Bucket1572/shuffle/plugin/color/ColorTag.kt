906ea0d71e20e084ba8de92a99cee7c40b644326
Bucket1572 <sout0811@naver.com>
2022-01-05 22:17:47 +0900

    Feat: Enum for colors

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/color/ColorTag.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/color/ColorTag.kt
@@ -1,4 +1,12 @@
 package io.github.Bucket1572.shuffle.plugin.color
 
-class ColorTag {
+import net.kyori.adventure.text.format.TextColor
+
+enum class ColorTag(val red: Int, val green: Int, val blue: Int) {
+    SCOREBOARD(255, 0, 0),
+    MINIGAME_TITLE(255, 255, 255),
+    MINIGAME_SUBTITLE(255, 215, 0),
+    MINIGAME_DESCRIPTION(255, 255, 255)
 }
+
+fun ColorTag.getTextColor(): TextColor = TextColor.color(red, green, blue)
