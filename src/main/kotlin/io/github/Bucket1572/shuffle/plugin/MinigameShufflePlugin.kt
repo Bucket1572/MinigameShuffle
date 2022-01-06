8adb484457ce3a76c51c5bcedc023c77726c7b85
Bucket1572 <sout0811@naver.com>
2022-01-06 23:52:17 +0900

    Test: 셜커 박스에 아이템 넣기 테스트; 성공

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/MinigameShufflePlugin.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/MinigameShufflePlugin.kt
@@ -1,6 +1,10 @@
 package io.github.Bucket1572.shuffle.plugin
 
+import io.github.Bucket1572.shuffle.plugin.minigame.prepare.PrepareUtility
 import io.github.monun.kommand.kommand
+import org.bukkit.Material
+import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
 class MinigameShufflePlugin: JavaPlugin() {
@@ -9,6 +13,5 @@ class MinigameShufflePlugin: JavaPlugin() {
     }
 
     private fun registerCommands() = kommand {
-
     }
 }
