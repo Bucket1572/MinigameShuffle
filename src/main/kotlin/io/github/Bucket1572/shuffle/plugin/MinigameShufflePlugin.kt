8d6ebab1998fd45bb9b3aff0ce052de9b0253be5
Bucket1572 <sout0811@naver.com>
2022-01-08 01:28:15 +0900

    Test: 첫 번째 미니게임 테스트 통과

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/MinigameShufflePlugin.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/MinigameShufflePlugin.kt
@@ -1,9 +1,15 @@
 package io.github.Bucket1572.shuffle.plugin
 
-import io.github.Bucket1572.shuffle.plugin.minigame.prepare.PrepareUtility
+import io.github.Bucket1572.shuffle.plugin.minigame.SplendidGems
+import io.github.Bucket1572.shuffle.plugin.minigame.description.DescriptionUtility
+import io.github.Bucket1572.shuffle.plugin.score.ScoreHandlingUtility
+import io.github.Bucket1572.shuffle.plugin.tag.ColorTag
+import io.github.Bucket1572.shuffle.plugin.tag.getTextColor
 import io.github.monun.kommand.kommand
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
 import org.bukkit.Material
-import org.bukkit.entity.Player
+import org.bukkit.enchantments.Enchantment
 import org.bukkit.inventory.ItemStack
 import org.bukkit.plugin.java.JavaPlugin
 
@@ -13,5 +19,12 @@ class MinigameShufflePlugin: JavaPlugin() {
     }
 
     private fun registerCommands() = kommand {
+        register("gems") {
+            executes {
+                ScoreHandlingUtility.initScoreBoard(server)
+                val minigame = SplendidGems(server)
+                minigame.start()
+            }
+        }
     }
 }
