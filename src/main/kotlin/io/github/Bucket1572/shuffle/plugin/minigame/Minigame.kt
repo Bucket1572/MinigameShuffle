c161c9ac6d7959c848c0933603a91180f6ecf7e7
Bucket1572 <sout0811@naver.com>
2022-01-08 01:26:10 +0900

    Fixed: Minigame base 조금 더 세분화

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/Minigame.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/Minigame.kt
@@ -1,11 +1,13 @@
 package io.github.Bucket1572.shuffle.plugin.minigame
 
+import io.github.Bucket1572.shuffle.plugin.minigame.cleanup.cleanUp
 import io.github.Bucket1572.shuffle.plugin.result.MinigameResult
 import io.github.Bucket1572.shuffle.plugin.tag.ColorTag
 import io.github.Bucket1572.shuffle.plugin.tag.getTextColor
 import io.github.monun.heartbeat.coroutines.HeartbeatScope
 import io.github.monun.heartbeat.coroutines.Suspension
 import kotlinx.coroutines.launch
+import net.kyori.adventure.bossbar.BossBar
 import net.kyori.adventure.text.Component
 import net.kyori.adventure.title.Title
 import org.bukkit.Server
@@ -13,7 +15,7 @@ import org.bukkit.entity.Player
 import java.time.Duration
 
 abstract class Minigame(
-    private val server: Server, private val name: String, private val summary: String,
+    private val server: Server, public val name: String, protected val summary: String,
 ) {
     fun start() {
         broadcast()
@@ -40,9 +42,22 @@ abstract class Minigame(
     abstract fun prepare()
 
     private fun startCountDown() {
+        val timeLength = 300
+        var remainingTime = timeLength
+        val bossBar = BossBar.bossBar(
+            Component.text(name).color(ColorTag.MINIGAME_TITLE.getTextColor()),
+            remainingTime / timeLength.toFloat(), BossBar.Color.GREEN, BossBar.Overlay.PROGRESS
+        )
         HeartbeatScope().launch {
             val suspension = Suspension()
-            suspension.delay(5 * 60 * 1000L)
+            server.showBossBar(bossBar)
+            repeat(timeLength) {
+                suspension.delay(1000L)
+                remainingTime--
+                if (remainingTime <= 0) bossBar.progress(0.0f)
+                else bossBar.progress(remainingTime / timeLength.toFloat())
+            }
+            server.hideBossBar(bossBar)
             end()
         }
     }
@@ -51,18 +66,18 @@ abstract class Minigame(
 
     private fun end() {
         server.onlinePlayers.forEach { player ->
-            run {
-                when (judge(player)) {
-                    MinigameResult.WIN -> winResponse(player)
-                    MinigameResult.LOSE -> loseResponse(player)
-                    MinigameResult.FAIL -> failResponse(player)
-                }
+            when (judge(player, getRankings())) {
+                MinigameResult.WIN -> winResponse(player)
+                MinigameResult.LOSE -> loseResponse(player)
+                MinigameResult.FAIL -> failResponse(player)
             }
         }
         cleanUp()
     }
 
-    abstract fun judge(player: Player): MinigameResult
+    abstract fun judge(player: Player, rankings: List<Player>): MinigameResult
+
+    abstract fun getRankings(): List<Player>
 
     abstract fun winResponse(player: Player)
 
@@ -70,5 +85,7 @@ abstract class Minigame(
 
     abstract fun failResponse(player: Player)
 
-    abstract fun cleanUp()
+    private fun cleanUp() {
+        server.cleanUp(this)
+    }
 }
