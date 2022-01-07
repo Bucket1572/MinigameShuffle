2c6ba30f20228710076e35db691188915c72be7c
Bucket1572 <sout0811@naver.com>
2022-01-08 01:23:46 +0900

    Feat: 새로운 색깔 태그 추가

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/tag/ColorTag.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/tag/ColorTag.kt
@@ -4,6 +4,12 @@ import net.kyori.adventure.text.format.TextColor
 
 enum class ColorTag(val red: Int, val green: Int, val blue: Int) {
     SCOREBOARD(255, 0, 0),
+    ALERT(255, 0, 0),
+    REWARD(0, 255, 0),
+    BUFFER(169, 169, 169),
+    WIN(0, 255, 0),
+    LOSE(169, 169, 169),
+    FAIL(255, 0, 0),
     MINIGAME_TITLE(255, 255, 255),
     MINIGAME_SUBTITLE(255, 215, 0),
     MINIGAME_DESCRIPTION(255, 255, 255)
