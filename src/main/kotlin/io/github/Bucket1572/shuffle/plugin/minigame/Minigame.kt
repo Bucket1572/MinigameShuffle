c50267190388739329a342e67a9987b8b9ca91b6
Bucket1572 <sout0811@naver.com>
2022-01-09 02:36:12 +0900

    Fixed: 미니게임 abstract class의 구조 변경; 공통적인 요소 묶어내기

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/Minigame.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/Minigame.kt
@@ -1,7 +1,13 @@
 package io.github.Bucket1572.shuffle.plugin.minigame
 
+import io.github.Bucket1572.shuffle.plugin.common.reward
 import io.github.Bucket1572.shuffle.plugin.minigame.cleanup.cleanUp
+import io.github.Bucket1572.shuffle.plugin.minigame.description.DescriptionUtility
+import io.github.Bucket1572.shuffle.plugin.minigame.prepare.PrepareUtility
+import io.github.Bucket1572.shuffle.plugin.minigame.prepare.feed
 import io.github.Bucket1572.shuffle.plugin.result.MinigameResult
+import io.github.Bucket1572.shuffle.plugin.score.fail
+import io.github.Bucket1572.shuffle.plugin.score.win
 import io.github.Bucket1572.shuffle.plugin.tag.ColorTag
 import io.github.Bucket1572.shuffle.plugin.tag.getTextColor
 import io.github.monun.heartbeat.coroutines.HeartbeatScope
@@ -9,13 +15,17 @@ import io.github.monun.heartbeat.coroutines.Suspension
 import kotlinx.coroutines.launch
 import net.kyori.adventure.bossbar.BossBar
 import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.format.TextDecoration
 import net.kyori.adventure.title.Title
+import org.bukkit.Material
 import org.bukkit.Server
 import org.bukkit.entity.Player
+import org.bukkit.inventory.ItemStack
 import java.time.Duration
 
 abstract class Minigame(
-    private val server: Server, public val name: String, protected val summary: String,
+    private val server: Server, val name: String, private val summary: String, private val iconMaterial: Material,
+    private val details: List<String>, private val failConditions: List<String>
 ) {
     fun start() {
         broadcast()
@@ -39,10 +49,17 @@ abstract class Minigame(
         server.broadcast(description)
     }
 
-    abstract fun prepare()
+    private fun prepare() {
+        val minigameIcon = PrepareUtility.setMinigameIcon(iconMaterial, name, summary)
+        val helperTools = getHelperTools()
+        val shulkerPackage = PrepareUtility.shulkerPackage(minigameIcon, helperTools)
+        server.feed(shulkerPackage)
+    }
+
+    abstract fun getHelperTools(): List<ItemStack>
 
     private fun startCountDown() {
-        val timeLength = 300
+        val timeLength = 10
         var remainingTime = timeLength
         val bossBar = BossBar.bossBar(
             Component.text(name).color(ColorTag.MINIGAME_TITLE.getTextColor()),
@@ -62,7 +79,16 @@ abstract class Minigame(
         }
     }
 
-    abstract fun getDescription(): Component
+    private fun getDescription(): Component {
+        val title = Component.text(name).style {
+            it.decoration(TextDecoration.BOLD, TextDecoration.State.TRUE)
+            it.color(ColorTag.MINIGAME_TITLE.getTextColor())
+        }
+        val rewards = getRewards()
+        return DescriptionUtility.generateDescription(title, details, failConditions, rewards)
+    }
+
+    abstract fun getRewards(): List<ItemStack>
 
     private fun end() {
         server.onlinePlayers.forEach { player ->
@@ -79,11 +105,20 @@ abstract class Minigame(
 
     abstract fun getRankings(): List<Player>
 
-    abstract fun winResponse(player: Player)
+    private fun winResponse(player: Player) {
+        player.sendActionBar(Component.text("승리!").color(ColorTag.WIN.getTextColor()))
+        player.win()
+        player.reward(getRewards())
+    }
 
-    abstract fun loseResponse(player: Player)
+    private fun loseResponse(player: Player) {
+        player.sendActionBar(Component.text("패배...").color(ColorTag.LOSE.getTextColor()))
+    }
 
-    abstract fun failResponse(player: Player)
+    private fun failResponse(player: Player) {
+        player.sendActionBar(Component.text("⚠ 실격 ⚠").color(ColorTag.FAIL.getTextColor()))
+        player.fail()
+    }
 
     private fun cleanUp() {
         server.cleanUp(this)
--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/SplendidGems.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/SplendidGems.kt
@@ -20,38 +20,25 @@ import org.bukkit.inventory.ItemStack
 
 class SplendidGems(private val server: Server):
     Minigame(
-        server, "반짝이는 보석들", "다이아몬드, 에메랄드와 자수정을 가장 많이 모으세요."
-    )
-{
-    override fun prepare() {
-        val minigameIcon = PrepareUtility.setMinigameIcon(Material.DIAMOND_ORE, name, summary)
-        val helperTools = listOf<ItemStack>()
-        val shulkerPackage = PrepareUtility.shulkerPackage(minigameIcon, helperTools)
-        server.feed(shulkerPackage)
-    }
-
-    override fun getDescription(): Component {
-        val title = Component.text(name).style {
-            it.decoration(TextDecoration.BOLD, TextDecoration.State.TRUE)
-            it.color(ColorTag.MINIGAME_TITLE.getTextColor())
-        }
-        val details = listOf(
+        server, "반짝이는 보석들", "다이아몬드, 에메랄드와 자수정을 가장 많이 모으세요.",
+        Material.DIAMOND_ORE,
+        listOf(
             "제한 시간 안에 다이아몬드, 에메랄드와 자수정 조각을 최대한 많이 모아야 합니다.",
             "제한 시간이 끝나면 각 플레이어가 가지고 있는 보석 개수를 계산합니다.",
             "그 결과, 가장 많은 보석을 가지고 있는 플레이어가 승리합니다."
-        )
-        val failConditions = listOf(
+        ),
+        listOf(
             "제한 시간 내에 보석을 하나도 모으지 못했을 경우"
         )
-        val rewards = getRewards()
-        return DescriptionUtility.generateDescription(title, details, failConditions, rewards)
+    )
+{
+    override fun getHelperTools(): List<ItemStack> {
+        return listOf()
     }
 
     override fun judge(player: Player, rankings: List<Player>): MinigameResult {
         val inventory = player.inventory
-        println("First")
         val playerHasJewelFlag = inventory.any { isJewel(it) }
-        println("Second")
         if (!playerHasJewelFlag) return MinigameResult.FAIL
 
         val playerJewelCount = countJewels(player)
@@ -67,21 +54,7 @@ class SplendidGems(private val server: Server):
         }
     }
 
-    override fun winResponse(player: Player) {
-        player.sendActionBar(Component.text("승리!").color(ColorTag.WIN.getTextColor()))
-        player.win()
-    }
-
-    override fun loseResponse(player: Player) {
-        player.sendActionBar(Component.text("패배...").color(ColorTag.LOSE.getTextColor()))
-    }
-
-    override fun failResponse(player: Player) {
-        player.sendActionBar(Component.text("⚠ 실격 ⚠").color(ColorTag.FAIL.getTextColor()))
-        player.fail()
-    }
-
-    private fun getRewards(): List<ItemStack> {
+    override fun getRewards(): List<ItemStack> {
         val diamondPickaxe = ItemStack(Material.DIAMOND_PICKAXE)
         diamondPickaxe.editMeta {
             it.addEnchant(Enchantment.LOOT_BONUS_BLOCKS, 2, false)
