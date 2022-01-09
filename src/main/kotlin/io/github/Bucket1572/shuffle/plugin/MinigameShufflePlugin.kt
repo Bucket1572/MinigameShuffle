786051f9aa25c249c79d975d914de680b7754223
Bucket1572 <sout0811@naver.com>
2022-01-09 22:57:03 +0900

    Test: 새로 추가한 미니게임 테스트; 통과 (숨바꼭질은 혼자 테스트하여 제대로 테스트 되지 않음)

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/MinigameShufflePlugin.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/MinigameShufflePlugin.kt
@@ -1,38 +1,59 @@
 package io.github.Bucket1572.shuffle.plugin
 
-import io.github.Bucket1572.shuffle.plugin.minigame.ClimbingContest
-import io.github.Bucket1572.shuffle.plugin.minigame.SplendidGems
-import io.github.Bucket1572.shuffle.plugin.minigame.description.DescriptionUtility
+import io.github.Bucket1572.shuffle.plugin.minigame.*
 import io.github.Bucket1572.shuffle.plugin.score.ScoreHandlingUtility
-import io.github.Bucket1572.shuffle.plugin.tag.ColorTag
-import io.github.Bucket1572.shuffle.plugin.tag.getTextColor
 import io.github.monun.kommand.kommand
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.format.TextDecoration
-import org.bukkit.Material
-import org.bukkit.enchantments.Enchantment
-import org.bukkit.inventory.ItemStack
+import org.bukkit.World
 import org.bukkit.plugin.java.JavaPlugin
+import java.util.*
 
 class MinigameShufflePlugin: JavaPlugin() {
+    val playAreaLowerLeft = mutableMapOf(Pair("x", 0.0), Pair("z", 0.0))
+    val playAreaUpperRight = mutableMapOf(Pair("x", 0.0), Pair("z", 0.0))
+    var playAreaWorld: World? = null
+
     override fun onEnable() {
         registerCommands()
     }
 
     private fun registerCommands() = kommand {
-        register("gems") {
-            executes {
-                ScoreHandlingUtility.initScoreBoard(server)
-                val minigame = SplendidGems(server)
-                minigame.start()
+        register("test") {
+            then(
+                "minigame" to dynamicByEnum(
+                    EnumSet.allOf(MinigameTag::class.java)
+                )
+            ) {
+                executes { context ->
+                    val worldBorder = location.world.worldBorder
+                    worldBorder.size = 100.0
+                    worldBorder.center = location.toBlockLocation()
+
+                    playAreaLowerLeft["x"] = worldBorder.center.x - 50.0
+                    playAreaLowerLeft["z"] = worldBorder.center.z - 50.0
+
+                    playAreaUpperRight["x"] = worldBorder.center.x + 50.0
+                    playAreaUpperRight["z"] = worldBorder.center.z + 50.0
+
+                    playAreaWorld = location.world
+
+                    val minigameTag = context.get<MinigameTag>("minigame")
+                    testMinigame(minigameTag)
+                }
             }
         }
-        register("climb") {
-            executes {
-                ScoreHandlingUtility.initScoreBoard(server)
-                val minigame = ClimbingContest(server)
-                minigame.start()
-            }
+    }
+
+    private fun testMinigame(minigameTag: MinigameTag) {
+        ScoreHandlingUtility.initScoreBoard(server)
+        val minigame = when (minigameTag) {
+            MinigameTag.GEMS -> SplendidGems(this)
+            MinigameTag.CLIMBING -> ClimbingContest(this)
+            MinigameTag.PUPPY -> CutePuppy(this)
+            MinigameTag.CAT -> CatServant(this)
+            MinigameTag.HIDE_AND_SEEK -> HideAndSeek(this)
+            MinigameTag.DELIVERY -> OcelotDelivery(this)
+            MinigameTag.ALCHEMY -> IntroductionToPotions(this)
         }
+        minigame.start()
     }
 }
