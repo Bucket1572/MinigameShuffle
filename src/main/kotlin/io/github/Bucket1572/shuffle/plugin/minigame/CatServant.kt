f60b5991f3a7d8119b8b0d1bf04c086fb83104c4
Bucket1572 <sout0811@naver.com>
2022-01-09 22:55:31 +0900

    Feat: 강아지/고양이 길들이기 미니게임 추가

--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/CatServant.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/CatServant.kt
@@ -10,27 +10,27 @@ import org.bukkit.event.Listener
 import org.bukkit.event.entity.EntityTameEvent
 import org.bukkit.inventory.ItemStack
 
-class CutePuppy(private val plugin: MinigameShufflePlugin):
+class CatServant(private val plugin: MinigameShufflePlugin):
     Minigame(
-        plugin.server, "귀여운 댕댕이", "가장 먼저 늑대를 길들이세요.",
-        Material.LEAD,
+        plugin.server, "집사 되기", "가장 먼저 고양이를 길들이세요.",
+        Material.WARPED_FUNGUS_ON_A_STICK,
         listOf(
-            "가장 먼저 늑대를 길들여야 합니다.",
-            "이전에 길들인 늑대는 인정되지 않습니다. 새로 늑대를 길들이세요."
+            "가장 먼저 고양이를 길들여야 합니다.",
+            "이전에 길들인 고양이는 인정되지 않습니다. 새로 고양이를 길들이세요."
         ),
         listOf(
-            "제한 시간 내에 늑대를 한 마리도 길들이지 못했을 경우"
+            "제한 시간 내에 고양이를 한 마리도 길들이지 못했을 경우"
         )
     ), Listener
 {
-    private val wolfTamingRanking = mutableListOf<Player>()
+    private val catTamingRanking = mutableListOf<Player>()
 
     override fun getHelperTools(): List<ItemStack> {
         return emptyList()
     }
 
     override fun additionalPreparation() {
-        wolfTamingRanking.clear()
+        catTamingRanking.clear()
         plugin.server.pluginManager.registerEvents(this, plugin)
     }
 
@@ -39,13 +39,13 @@ class CutePuppy(private val plugin: MinigameShufflePlugin):
     }
 
     override fun judge(player: Player, rankings: List<Player>): MinigameResult {
-        if (player !in wolfTamingRanking) return MinigameResult.FAIL
-        if (player == wolfTamingRanking[0]) return MinigameResult.WIN
+        if (player !in catTamingRanking) return MinigameResult.FAIL
+        if (player == catTamingRanking[0]) return MinigameResult.WIN
         return MinigameResult.LOSE
     }
 
     override fun getRankings(): List<Player> {
-        return wolfTamingRanking
+        return catTamingRanking
     }
 
     override fun additionalCleanUp() {
@@ -53,18 +53,15 @@ class CutePuppy(private val plugin: MinigameShufflePlugin):
     }
 
     @EventHandler
-    fun onTamingWolf(event: EntityTameEvent) {
-        val tamedAnimal = event.entityType
-
-        if (tamedAnimal != EntityType.WOLF) return
+    fun onBecomingServant(event: EntityTameEvent) {
         if (!isValidToRegister(event)) return
 
-        wolfTamingRanking.add(event.owner as Player)
+        catTamingRanking.add(event.owner as Player)
     }
 
     private fun isValidToRegister(event: EntityTameEvent): Boolean {
-        return (event.entityType == EntityType.WOLF)
+        return (event.entityType == EntityType.CAT)
                 && (event.owner is Player)
-                && ((event.owner as Player) !in wolfTamingRanking)
+                && ((event.owner as Player) !in catTamingRanking)
     }
 }
--- src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/CutePuppy.kt
+++ src/main/kotlin/io/github/Bucket1572/shuffle/plugin/minigame/CutePuppy.kt
@@ -1,4 +1,67 @@
 package io.github.Bucket1572.shuffle.plugin.minigame
 
-class CutePuppy {
+import io.github.Bucket1572.shuffle.plugin.MinigameShufflePlugin
+import io.github.Bucket1572.shuffle.plugin.result.MinigameResult
+import org.bukkit.Material
+import org.bukkit.entity.EntityType
+import org.bukkit.entity.Player
+import org.bukkit.event.EventHandler
+import org.bukkit.event.Listener
+import org.bukkit.event.entity.EntityTameEvent
+import org.bukkit.inventory.ItemStack
+
+class CutePuppy(private val plugin: MinigameShufflePlugin):
+    Minigame(
+        plugin.server, "귀여운 댕댕이", "가장 먼저 늑대를 길들이세요.",
+        Material.LEAD,
+        listOf(
+            "가장 먼저 늑대를 길들여야 합니다.",
+            "이전에 길들인 늑대는 인정되지 않습니다. 새로 늑대를 길들이세요."
+        ),
+        listOf(
+            "제한 시간 내에 늑대를 한 마리도 길들이지 못했을 경우"
+        )
+    ), Listener
+{
+    private val wolfTamingRanking = mutableListOf<Player>()
+
+    override fun getHelperTools(): List<ItemStack> {
+        return emptyList()
+    }
+
+    override fun additionalPreparation() {
+        wolfTamingRanking.clear()
+        plugin.server.pluginManager.registerEvents(this, plugin)
+    }
+
+    override fun getRewards(): List<ItemStack> {
+        return emptyList()
+    }
+
+    override fun judge(player: Player, rankings: List<Player>): MinigameResult {
+        if (player !in wolfTamingRanking) return MinigameResult.FAIL
+        if (player == wolfTamingRanking[0]) return MinigameResult.WIN
+        return MinigameResult.LOSE
+    }
+
+    override fun getRankings(): List<Player> {
+        return wolfTamingRanking
+    }
+
+    override fun additionalCleanUp() {
+        EntityTameEvent.getHandlerList().unregister(this)
+    }
+
+    @EventHandler
+    fun onTamingWolf(event: EntityTameEvent) {
+        if (!isValidToRegister(event)) return
+
+        wolfTamingRanking.add(event.owner as Player)
+    }
+
+    private fun isValidToRegister(event: EntityTameEvent): Boolean {
+        return (event.entityType == EntityType.WOLF)
+                && (event.owner is Player)
+                && ((event.owner as Player) !in wolfTamingRanking)
+    }
 }
